b4a4ce104cfca6a88c12e0a1fa0880f4ea402b24
Lenoci Gaetano <gaetano.lenoci@vaimoo.com>
2023-09-05 10:15:19 +0200

    wip

--- Src/Yelper/Services/Identity/Identity.API/Program.cs
+++ Src/Yelper/Services/Identity/Identity.API/Program.cs
@@ -1,6 +1,13 @@
+using Identity.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var provider = builder.Services.BuildServiceProvider(); //correggere
+var configuration = provider.GetService<IConfiguration>();
+
 // Add services to the container.
+
+builder.Services.AddInfrastructure(configuration!);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
